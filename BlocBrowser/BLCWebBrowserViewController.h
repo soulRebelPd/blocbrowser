5028f757c1e430979af02dd39cd5c892917bddc6
Corey Norford <cnorford@rocketmail.com>
2015-02-25 11:33:15 -0500

    Functional web browswer

---
+++ BlocBrowser/BLCWebBrowserViewController.h
@@ -0,0 +1,13 @@
+//
+//  BLCWebBrowserViewController.h
+//  BlocBrowser
+//
+//  Created by Corey Norford on 2/25/15.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLCWebBrowserViewController : UIViewController
+
+@end
